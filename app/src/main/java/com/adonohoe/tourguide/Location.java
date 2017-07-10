2491020e2bfe9863c5cb0060b5686b5fe3af7214
ashleydonohoe <ashleyrdonohoe@gmail.com>
2017-07-09 23:09:39 -0400

    working on initial layout

--- app/src/main/java/com/adonohoe/tourguide/Location.java
+++ app/src/main/java/com/adonohoe/tourguide/Location.java
@@ -1,20 +1,30 @@
 package com.adonohoe.tourguide;
 
-/**
- * Created by ashleydonohoe on 7/7/17.
- */
-
 public class Location {
     // private variables
     private String mLocationName;
     private String mLocationAddress;
+    private String mLocationDescription;
     private String mLocationPhoneNumber;
+    // used technique of checking if location has an image taken from Miwok app
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED = -1;
 
-    // Constructor
-    public Location(String locationName, String locationAddress, String locationPhoneNumber) {
+    // Constructor without image
+    public Location(String locationName, String locationAddress, String locationDescription, String locationPhoneNumber) {
         mLocationName = locationName;
         mLocationAddress = locationAddress;
+        mLocationDescription = locationDescription;
         mLocationPhoneNumber = locationPhoneNumber;
+    }
+
+    // Constructor with image
+    public Location(String locationName, String locationAddress, String locationDescription, String locationPhoneNumber, int imgResourceId) {
+        mLocationName = locationName;
+        mLocationAddress = locationAddress;
+        mLocationDescription = locationDescription;
+        mLocationPhoneNumber = locationPhoneNumber;
+        mImageResourceId = imgResourceId;
     }
 
     // Gets location name
@@ -25,5 +35,25 @@ public class Location {
     // Gets location address
     public String getLocationAddress() {
         return mLocationAddress;
+    }
+
+    // Get location phone number
+    public String getLocationPhoneNumber() {
+        return mLocationPhoneNumber;
+    }
+
+    // Get location description
+    public String getLocationDescription() {
+        return mLocationDescription;
+    }
+
+    // Get image resource; adapted from Miwok app
+    public int getmImageResourceId() {
+        return mImageResourceId;
+    }
+
+    // Check if image exists for Location; adapted from Miwok app
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 }
---
+++ app/src/main/java/com/adonohoe/tourguide/LocationAdapter.java
@@ -0,0 +1,8 @@
+package com.adonohoe.tourguide;
+
+/**
+ * Created by ashleydonohoe on 7/9/17.
+ */
+
+public class LocationAdapter {
+}
