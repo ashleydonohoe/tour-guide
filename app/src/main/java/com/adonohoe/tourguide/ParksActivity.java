a3e858547a93ee1d471882b001aeab8616e8b55d
ashleydonohoe <ashleyrdonohoe@gmail.com>
2017-07-10 23:00:47 -0400

    added images

--- app/src/main/java/com/adonohoe/tourguide/ParksActivity.java
+++ app/src/main/java/com/adonohoe/tourguide/ParksActivity.java
@@ -15,9 +15,9 @@ public class ParksActivity extends AppCompatActivity {
 
         // Create list of parks
         ArrayList<Location> parks = new ArrayList<Location>();
-        parks.add(new Location(R.string.carriage_way_name, R.string.carriage_way_address, R.string.carriage_way_phone, R.string.carriage_way_desc, R.drawable.placeholder));
+        parks.add(new Location(R.string.carriage_way_name, R.string.carriage_way_address, R.string.carriage_way_phone, R.string.carriage_way_desc));
         parks.add(new Location(R.string.finley_name, R.string.finley_address, R.string.finley_phone, R.string.finley_desc));
-        parks.add(new Location(R.string.nature_center_name, R.string.nature_center_address, R.string.nature_center_phone, R.string.nature_center_desc));
+        parks.add(new Location(R.string.nature_center_name, R.string.nature_center_address, R.string.nature_center_phone, R.string.nature_center_desc, R.drawable.nature_center));
         parks.add(new Location(R.string.miami_meadows_name, R.string.miami_meadows_address, R.string.miami_meadows_phone, R.string.miami_meadows_desc));
 
         LocationAdapter adapter = new LocationAdapter(this, parks);
